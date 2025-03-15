d1d057ad261b6de534af090ec3ee9c9cf5705d83
Hassan Tariq Murtaza <hassantarique@hotmail.com>
2025-03-16 00:49:41 +0400

    Bootstrap and css classes added

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="./css/globals.css" />
+            </Head>
+            <body>
+                <div className="container">
+                    <Main />
+                </div>
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
