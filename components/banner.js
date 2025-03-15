1246bd224a361183d88f894a09f9e3b01a138b62
Hassan Tariq Murtaza <hassantarique@hotmail.com>
2025-03-15 20:47:20 +0400

    Adding New Components

---
+++ components/banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+	return (
+		<header>
+			<div>
+				<img src="./GloboLogo.png" alt="logo" />
+			</div>
+			<div>
+				Providing house all over the world
+			</div>
+		</header>
+	);
+};
+
+export default Banner;
